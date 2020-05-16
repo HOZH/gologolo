e726c1a0867141375a94827e16171720d86b14c6
HOZH <hong.zheng@stonybrook.edu>
2020-05-16 14:45:36 -0400

    doing a draft for showing draggble, multi item logo

---
+++ draft/draft/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Temp from './components/Temp'
+
+function App() {
+  return  <Temp/>
+}
+
+export default App;
